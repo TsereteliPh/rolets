6e755bc7897910c3f060c2f6bae29c509945a0fb
TsereteliPh <filipptsereteli@yandex.ru>
2024-08-27 13:18:55 +0300

    add title

--- layouts/partials/title.php
+++ layouts/partials/title.php
@@ -1,8 +1,25 @@
 <?php
 if ( ! empty( $args['title']['text'] ) ) {
-	echo sprintf( '<%1$s class="title %2$s">%3$s</%1$s>',
-		$args['title']['type'],
+	$format = '
+		<div class="title %1$s">
+			<div class="title__small-text">%2$s</div>
+
+			<%3$s class="title__text">%4$s</%3$s>
+	';
+
+	if ( $args['title']['link']['url'] ) {
+		$format .= '<a href="%5$s" class="btn-underline title__link" target="%6$s">%7$s</a>';
+	}
+
+	$format .= '</div>';
+	echo sprintf(
+		$format,
 		$args['class'],
-		$args['title']['text']
+		$args['title']['small-text'],
+		$args['title']['type'],
+		$args['title']['text'],
+		$args['title']['link']['url'],
+		$args['title']['link']['target'],
+		$args['title']['link']['title'],
 	);
 }
