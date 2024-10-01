b0ddaefd07967df9fef64453492fdb5b10aa25eb
TsereteliPh <filipptsereteli@yandex.ru>
2024-10-01 14:50:38 +0300

    add and done .order-production

---
+++ layouts/blocks/order-production/template.php
@@ -0,0 +1,64 @@
+<?php
+	$bid = get_sub_field( 'bid' );
+	$measurer = get_sub_field( 'measurer' );
+	$calculator = get_sub_field( 'calculator' );
+?>
+
+<section class="order-production">
+	<div class="container">
+		<?php get_template_part( '/layouts/partials/title', null, array(
+			'class' => 'order-production__title',
+			'title' => get_sub_field( 'title' ),
+		) ); ?>
+
+		<div class="order-production__content">
+			<?php if ( $bid['text'] || $bid['link'] ) : ?>
+				<div class="order-production__box order-production__box--bid">
+					<div class="order-production__box-content">
+						<div class="order-production__box-text"><?php echo $bid['text']; ?></div>
+
+						<?php if ( $bid['link'] ) : ?>
+							<button class="btn btn--thick order-production__box-button" type="button" data-fancybox data-src="#callback">
+								<svg width="16" height="16"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow"></use></svg>
+
+								<?php echo $bid['link']; ?>
+							</button>
+						<?php endif; ?>
+					</div>
+				</div>
+			<?php endif; ?>
+
+			<?php if ( $measurer['text'] || $measurer['link'] ) : ?>
+				<div class="order-production__box order-production__box--measurer">
+					<div class="order-production__box-content">
+						<div class="order-production__box-text"><?php echo $measurer['text']; ?></div>
+
+						<?php if ( $measurer['link'] ) : ?>
+							<button class="btn btn--thick order-production__box-button" type="button" data-fancybox data-src="#measure">
+								<svg width="16" height="16"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow"></use></svg>
+
+								<?php echo $measurer['link']; ?>
+							</button>
+						<?php endif; ?>
+					</div>
+				</div>
+			<?php endif; ?>
+
+			<?php if ( $calculator['text'] || $calculator['link'] ) : ?>
+				<div class="order-production__box order-production__box--calculator">
+					<div class="order-production__box-content">
+						<div class="order-production__box-text"><?php echo $calculator['text']; ?></div>
+
+						<?php if ( $calculator['link'] ) : ?>
+							<a href="<?php echo $calculator['link']['url']; ?>" class="btn btn--thick order-production__box-button" target="<?php echo $calculator['link']['target']; ?>">
+								<svg width="16" height="16"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow"></use></svg>
+
+								<?php echo $calculator['link']['title']; ?>
+							</a>
+						<?php endif; ?>
+					</div>
+				</div>
+			<?php endif; ?>
+		</div>
+	</div>
+</section>
