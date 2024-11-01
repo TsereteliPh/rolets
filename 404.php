eb10712010ab1726bb60dfe89dcc4f8cef00bb6c
TsereteliPh <filipptsereteli@yandex.ru>
2024-11-01 13:43:40 +0300

    add and done 404 page

--- 404.php
+++ 404.php
@@ -2,11 +2,18 @@
 
 <section class="not-found">
 	<div class="container not-found__container">
+		<h1 class="title__text not-found__title">СТРАНИЦА НЕ НАЙДЕНА</h1>
+
 		<div class="not-found__text">
-			<h1 class="not-found__title">СТРАНИЦА НЕ НАЙДЕНА</h1>
-			<p>Такой страницы не существует.</p>
-			<p>Воспользуйтесь поиском по сайту.</p>
+			К сожалению, страница, которую вы ищете, не существует. Возможно, она была удалена, или вы неверно ввели адрес.<br>
+			<br>
+			Пожалуйста, перейдите на главную страницу и продолжите поиск нужной информации.
 		</div>
+
+		<a href="<?php echo bloginfo( 'url' ); ?>" class="btn not-found__link">
+			На главную
+			<svg width="12" height="12"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow"></use></svg>
+		</a>
 	</div>
 </section>
 
