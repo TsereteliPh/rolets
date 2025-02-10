afcf511194100b33a860cfe287f0b701855077b2
TsereteliPh <filipptsereteli@yandex.ru>
2025-02-10 18:22:11 +0300

    edit .alutech-calc

--- layouts/blocks/alutech-calc/template.php
+++ layouts/blocks/alutech-calc/template.php
@@ -9,6 +9,15 @@
 	</div>
 </section>
 
-<script>
-	!function(){var s="",h="",m=window.location.protocol+"//"+window.location.host+window.location.pathname;function g(t,e){e||(e=window.location.href),t=t.replace(/[\[\]]/g,"\\$&");var a=new RegExp("[?&]"+t+"(=([^&#]*)|&|#|$)").exec(e);return a?a[2]?decodeURIComponent(a[2].replace(/\+/g," ")):"":null}document.addEventListener("DOMContentLoaded",function(){s=g("alutcalc")||s;var t=g("alutsize")||"",a=g("alutpanels")||"",o=g("alutseries")||"",l=g("alutmount")||"",n=g("alutbox")||"",c=g("alutcolor")||"",i=g("alutcontrol")||"",u=g("alutoptions")||"";if(h=g("alutregion")||h,null!==(e=document.getElementById("alutech-calc"))){var r=document.createElement("iframe"),d="alutech-iframe";r.name="alutech_iframe";r.src="//calc.alute.ch/#"+s+"?alutsize="+t+"&email=&url="+m+"&css=&calc_metrika=&lang=ru&type=&sap=&calc=[]&alutpanels="+a+"&alutseries="+o+"&alutmount="+l+"&alutbox="+n+"&alutcolor="+c+"&alutcontrol="+i+"&alutoptions="+u+"&alutregion="+h,r.width="100%",r.height=0,r.id=d,r.scrolling="no",r.frameBorder=0,e.appendChild(r),window.addEventListener("message",function(e){e.data.height&&"number"==typeof e.data.height&&document.getElementById(d)&&(document.getElementById(d).height=e.data.height),e.data.run&&"string"==typeof e.data.run&&document.getElementById(d)&&new Function(e.data.run)()})}})}();
-</script>
+<?php
+	$types = get_sub_field( 'types' );
+	$email = get_sub_field( 'email' );
+?>
+
+<?php if ( ! empty( $email ) ) : ?>
+	<script>
+		const calc_types = "<?php echo implode( ',', $types ); ?>";
+		const calc_home = calc_types ? calc_types.split(',')[0].slice(1, -1) : '';
+		!function(){var s=calc_home,h="",m=window.location.protocol+"//"+window.location.host+window.location.pathname;function g(t,e){e||(e=window.location.href),t=t.replace(/[\[\]]/g,"\\$&");var a=new RegExp("[?&]"+t+"(=([^&#]*)|&|#|$)").exec(e);return a?a[2]?decodeURIComponent(a[2].replace(/\+/g," ")):"":null}document.addEventListener("DOMContentLoaded",function(){s=g("alutcalc")||s;var t=g("alutsize")||"",a=g("alutpanels")||"",o=g("alutseries")||"",l=g("alutmount")||"",n=g("alutbox")||"",c=g("alutcolor")||"",i=g("alutcontrol")||"",u=g("alutoptions")||"";if(h=g("alutregion")||h,null!==(e=document.getElementById("alutech-calc"))){var r=document.createElement("iframe"),d="alutech-iframe";r.name="alutech_iframe";r.src="//calc.alute.ch/#"+s+"?alutsize="+t+"&email=<?php echo $email; ?>&url="+m+"&css=&calc_metrika=&lang=ru&type=&sap=&calc=[" + calc_types + "]&alutpanels="+a+"&alutseries="+o+"&alutmount="+l+"&alutbox="+n+"&alutcolor="+c+"&alutcontrol="+i+"&alutoptions="+u+"&alutregion="+h,r.width="100%",r.height=0,r.id=d,r.scrolling="no",r.frameBorder=0,e.appendChild(r),window.addEventListener("message",function(e){e.data.height&&"number"==typeof e.data.height&&document.getElementById(d)&&(document.getElementById(d).height=e.data.height),e.data.run&&"string"==typeof e.data.run&&document.getElementById(d)&&new Function(e.data.run)()})}})}();
+	</script>
+<?php endif; ?>
