30162deb50407e8b8cc721f12d651acf1b3bf335
TsereteliPh <filipptsereteli@yandex.ru>
2025-02-28 15:41:06 +0300

    add and done .project-links

---
+++ layouts/blocks/project-links/template.php
@@ -0,0 +1,46 @@
+<?php
+	$title = get_sub_field( 'title' );
+	$projects = get_sub_field( 'projects' );
+?>
+<section class="project-links">
+	<div class="container">
+		<?php get_template_part( '/layouts/partials/title', null, array(
+			'class' => 'project-links__title',
+			'title' => get_sub_field( 'title' )
+		) ); ?>
+
+        <?php if ( $projects ) : ?>
+			<ul class="reset-list project-links__list">
+				<?php foreach ( $projects as $post ) : ?>
+					<?php setup_postdata( $post ); ?>
+
+					<li class="project-links__item">
+						<?php if ( get_field( 'new' ) ) : ?>
+							<div class="project-links__item-new">Новый проект</div>
+						<?php endif; ?>
+
+						<div class="project-links__item-img">
+							<?php
+								if ( has_post_thumbnail() ) {
+									the_post_thumbnail( 'large' );
+								} else {
+									echo wp_get_attachment_image( 31, 'large', false );
+								}
+							?>
+						</div>
+
+						<div class="project-links__item-info">
+							<a href="<?php echo $title['link']['url'] . '#' . 'project-' .  get_the_ID(); ?>" class="project-links__item-link">
+								<?php the_title(); ?>
+							</a>
+
+							<div class="project-links__item-price"><?php echo number_format( get_field( 'price' ), 0, ',', ' ' ); ?> руб.</div>
+						</div>
+					</li>
+				<?php endforeach; ?>
+
+				<?php wp_reset_postdata(); ?>
+			</ul>
+		<?php endif; ?>
+	</div>
+</section>
